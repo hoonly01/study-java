f2d84e6b6bdc187ffc9d45ba443495366d828b9d
hoon <hoonly01@naver.com>
2024-09-28 20:34:59 +0900

    0928 day 1

---
+++ java-start/src/CommentJava.java
@@ -0,0 +1,13 @@
+public class CommentJava {
+    public static void main(String[] args) {
+        System.out.println("hello java1"); // hello jaca1을 출력
+        // System.out.println("hello java 2");
+
+        /*
+        System.out.println("hello java 3");
+        System.out.println("hello java 4");
+        */
+
+
+    }
+}
