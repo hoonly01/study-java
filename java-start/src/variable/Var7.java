0dc988abe8284d0eec624d18d23c0e2868b40223
hoon <hoonly01@naver.com>
2024-09-28 20:48:32 +0900

    0928 day 1

---
+++ java-start/src/variable/Var7.java
@@ -0,0 +1,21 @@
+package variable;
+
+public class Var7 {
+    public static void main(String[] args) {
+        int a = 100;
+        double b = 10.5;
+        boolean c = true;
+        char d = 'a';
+        String e = "Hello java";
+
+        int f = 'a'; // O
+        // int f = "a"; // X
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
