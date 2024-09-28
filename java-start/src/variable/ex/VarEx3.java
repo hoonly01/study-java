2a367b18cf2cae32c98fe7e2a5893e5904b3eef5
hoon <hoonly01@naver.com>
2024-09-28 21:42:16 +0900

    0928 day 1

---
+++ java-start/src/variable/ex/VarEx3.java
@@ -0,0 +1,13 @@
+package variable.ex;
+
+import java.util.Arrays;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long longVar = 10000000000L;
+        System.out.println("longVar = " + longVar);
+
+        boolean booleanValue = true;
+        System.out.println("booleanValue = " + booleanValue);
+    }
+}
