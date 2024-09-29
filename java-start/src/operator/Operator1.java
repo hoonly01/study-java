8a223dffd95c73391e1c3a26a2a91ab1ac82fad9
hoon <hoonly01@naver.com>
2024-09-29 18:47:45 +0900

    0929 day 2

---
+++ java-start/src/operator/Operator1.java
@@ -0,0 +1,21 @@
+package operator;
+
+public class Operator1 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+
+        int sum = a + b;
+        System.out.println("a + b = " + a + b);
+        System.out.println("sum = " + sum);
+
+        int diff = a -b;
+        System.out.println("diff = " + diff);
+
+        int div = a / b;
+        System.out.println("div = " + div);
+
+        int mod = a % b;
+        System.out.println("mod = " + mod);
+    }
+}
